40cb81b71d28c2893844dcc984d3dcc9efca3ff6
Mathias Kahl <mk@kialo.com>
2023-08-07 15:16:28 +0200

    PM-41849 improved sync script
    Files that are deleted in the source repo should also be deleted from the copy in moodle.

--- development/sync.sh
+++ development/sync.sh
@@ -1,4 +1,4 @@
 # syncs the content of the development version of this plugin to the copy in the docker moodle installation (/moodle/mod/kialo)
 cd ..
-rsync -a --exclude development . ./development/moodle/mod/kialo
+rsync -a --delete --exclude development . ./development/moodle/mod/kialo
 echo "Synced plugin."
