13f420225ed1b5ab9352f9f6bcbb5b347b83af5d
Mathias Kahl <mk@kialo.com>
2023-08-10 10:09:44 +0200

    PM-42225 improved bundle.sh

--- development/bundle.sh
+++ development/bundle.sh
@@ -1,4 +1,10 @@
 # bundle kialo folder into a zip file (excluding development-related files)
 # Usage: ./bundle.sh
+
+# Ensure that the version in moodle is up-to-date. This corresponds to our release version.
+./sync.sh
+
 cd moodle/mod
-zip -r ../../mod_kialo.zip kialo
+rm ../../mod_kialo.zip
+zip -qr ../../mod_kialo.zip kialo
+echo "Bundle created: mod_kialo.zip"
