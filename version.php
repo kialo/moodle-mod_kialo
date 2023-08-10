02d6483402c35a16a1e644dc5eec6261b9d8c1f7
Mathias Kahl <mk@kialo.com>
2023-08-10 13:47:31 +0200

    PM-42133 incremented version to v0.1.7

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_kialo';
-$plugin->release = '0.1.6';
-$plugin->version = 2023072406;
+$plugin->release = '0.1.7';
+$plugin->version = 2023081001;
 $plugin->requires = 2019111800;
 $plugin->maturity = MATURITY_ALPHA;
