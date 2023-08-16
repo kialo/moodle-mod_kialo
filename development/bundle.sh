65c848313a1619998af029336f38422806a60260
Mathias Kahl <mk@kialo.com>
2023-08-16 11:27:38 +0200

    PM-42325 exclude dev requirements in bundle

--- development/bundle.sh
+++ development/bundle.sh
@@ -1,10 +1,23 @@
 # bundle kialo folder into a zip file (excluding development-related files)
 # Usage: ./bundle.sh
 
+# Only install production dependencies
+cd .. # cd into root of repository
+rm -rf vendor/
+composer install --no-dev --no-scripts --prefer-dist --no-interaction
+
 # Ensure that the version in moodle is up-to-date. This corresponds to our release version.
+cd development
 ./sync.sh
 
+# Create a new ZIP file
+rm mod_kialo.zip
 cd moodle/mod
-rm ../../mod_kialo.zip
 zip -qr ../../mod_kialo.zip kialo
+
+# restore full dependencies (including dev dependencies)
+cd ../../..
+composer install
+
+echo "-----------------------------"
 echo "Bundle created: mod_kialo.zip"
