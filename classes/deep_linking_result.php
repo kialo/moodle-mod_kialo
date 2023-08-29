19e55327eb2118d0d6071c984ed1a8799f1fd15f
Mathias Kahl <mk@kialo.com>
2023-08-29 10:58:01 +0200

    PM-42402 fixing linting issues

--- classes/deep_linking_result.php
+++ classes/deep_linking_result.php
@@ -1,7 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace mod_kialo;
 
+/**
+ * Data container used for the LTI implementation.
+ *
+ * @package    mod_kialo
+ * @category   activity
+ * @copyright  2023 Kialo GmbH
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class deep_linking_result {
     /**
      * Unique string identifier that was sent by our Moodle plugin when the activity was created.
--- classes/kialo_config.php
+++ classes/kialo_config.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 
 namespace mod_kialo;
 
@@ -10,6 +25,15 @@ use OAT\Library\Lti1p3Core\Security\Key\KeyChainInterface;
 use OAT\Library\Lti1p3Core\Security\Key\KeyInterface;
 use OAT\Library\Lti1p3Core\Tool\Tool;
 
+
+/**
+ * Defines capabilities for the Kialo activity module.
+ *
+ * @package    mod_kialo
+ * @category   activity
+ * @copyright  2023 Kialo GmbH
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class kialo_config {
     private static $instance = null;
 
