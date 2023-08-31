d4a39cdde8abc3b875f03f7e058ec9a3a6f1898d
Mathias Kahl <mk@kialo.com>
2023-08-31 16:45:29 +0200

    PM-42402 v1.0.4 - added missing files
    Plugin submission complained about these files missing.

---
+++ db/upgrade.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Upgrade steps for the Kialo activity module.
+ *
+ * @package    mod_kialo
+ * @copyright  2023 onwards, Kialo GmbH <support@kialo-edu.com>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Custom upgrade steps.
+ * @param int $oldversion
+ * @return void
+ */
+function xmldb_kialo_upgrade($oldversion = 0) {
+    // This is the first public version, so there is nothing to do yet.
+}
