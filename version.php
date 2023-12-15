a281f0c7248b11f429582dcb78717563f3e166a0
Mathias Kahl <mk@kialo.com>
2023-12-15 09:03:37 +0100

    PM-43525 v1.0.26 improved PHP 8.3 compatibility
    Starting from PHP 8.3 dynamic class property declares are deprecated. Added inspection and remove instances of this in our code.

--- version.php
+++ version.php
@@ -28,8 +28,8 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_kialo';
 
 // See https://moodledev.io/docs/apis/commonfiles/version.php.
-$plugin->version = 2023112901;  // Must be incremented for each new release!
-$plugin->release = '1.0.25';    // Semantic version.
+$plugin->version = 2023121501;  // Must be incremented for each new release!
+$plugin->release = '1.0.26';    // Semantic version.
 
 // Officially we require PHP 7.4. The first Moodle version that requires this as a minimum is Moodle 4.1.
 // But technically this plugin also runs on older Moodle versions, as long as they run on PHP 7.4,
