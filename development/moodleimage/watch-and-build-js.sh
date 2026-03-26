9eab54eb8ad8689762fd7650cbaf18fca6b61721
Marius Gawrisch <mg@kialo.com>
2026-03-26 11:53:52 +0100

    PM-48569 Use public prefix for mod directory

--- development/moodleimage/watch-and-build-js.sh
+++ development/moodleimage/watch-and-build-js.sh
@@ -4,8 +4,8 @@
 export NVM_DIR=$HOME/.nvm
 . $NVM_DIR/nvm.sh
 
-cd /var/www/html/mod/kialo
+cd /var/www/html/public/mod/kialo
 
-while inotifywait -r -e modify,create,delete,move /var/www/html/mod/kialo/amd/src; do
+while inotifywait -r -e modify,create,delete,move /var/www/html/public/mod/kialo/amd/src; do
   npx grunt amd;
 done
